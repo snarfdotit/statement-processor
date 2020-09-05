53e65c55373a0055b13d0b9e799af7464e627e59
frans <frans.wessels@snarf.it>
2020-09-05 14:27:52 +0200

    spring.io default creator

---
+++ src/main/java/it/snarf/rabo/statementprocessor/statementprocessor/StatementprocessorApplication.java
@@ -0,0 +1,13 @@
+package it.snarf.rabo.statementprocessor.statementprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatementprocessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatementprocessorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/snarf/rabo/statementprocessor/statementprocessor/StatementprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package it.snarf.rabo.statementprocessor.statementprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatementprocessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
